9eb55adc028dfb62fef2c7ab9d1071aa8c8fa31d
Crimsonable <broccoliology@163.com>
2022-11-21 00:36:07 +0800

    continue adding feature to expr

--- core/expr.h
+++ core/expr.h
@@ -8,15 +8,25 @@ namespace Hexer {
 
 enum class MemPolicy { INPLACE, OUTPLACE };
 
-template <Device device, typename Derived, typename ParamTuple = std::tuple<>>
-class CrtpExprBase {
+template <Device device, typename Derived>
+class CrtpExprBase<device, Derived, void, void> {
+public:
+  Derived *cast_to() { return static_cast<Derived *>(this); }
+
+  template <typename... Args> auto eval(Args &&...args) {
+    return cast_to()->run(std::forward<Args>(args)...);
+  }
+};
+
+template <Device device, typename Derived, typename ParamTuple>
+class CrtpExprBase<device, Derived, void, ParamTuple> {
 private:
   ParamTuple _args;
 
 public:
   CrtpExprBase() : _args(std::tuple<>()) {}
 
-  CrtpExprBase(const ParamTuple &params) : _args(params) {}
+  CrtpExprBase(ParamTuple &&params) : _args(params) {}
 
   Derived *cast_to() { return static_cast<Derived *>(this); }
 
@@ -24,28 +34,63 @@ public:
     return cast_to()->run(std::forward<Args>(args)...);
   }
 
-  void allocSpace(size_t size) { return cast_to()->alloc(size); }
+  auto getArgs() { return _args; }
 
-  template <typename... Args>
-    requires(std::tuple_size_v<ParamTuple> + sizeof...(Args) <=
-             Meta::traits<decltype(&Derived::eval)>::param_size)
-  auto operator()(Args &&...args) {
-    using p_type = decltype(std::tuple_cat(
-        _args, std::make_tuple(std::forward<Args>(args)...)));
-    return CrtpExprBase<device, Derived, p_type>(
-        std::tuple_cat(_args, std::make_tuple(std::forward<Args>(args)...)));
+  template <typename... Args> auto operator()(Args &&...args) {
+    auto new_args = std::tuple_cat(
+        _args, std::make_tuple(Meta::ref_helper(std::forward<Args>(args))...));
+    using p_type = decltype(new_args);
+    if constexpr (std::tuple_size_v<p_type> <
+                  Meta::traits<decltype(&Derived::eval)>::param_size)
+      return CrtpExprBase<device, Derived, void, p_type>(new_args);
+    else
+      return Meta::tuple_apply(&Derived::eval, cast_to(), new_args);
   }
 
-  // template <typename... Args>
-  // requires {
-  //   std::tuple_size_v<ParamTuple> + sizeof...(Args) ==
-  //       Meta::traits<decltype(&Derived::eval)>::param_size
-  // } auto operator()(Args &&...args) {
-  //   return Meta::tuple_apply(cast_to()->eval, _args);
-  // }
+  template <typename Rhs_Derived, typename PTuple>
+  auto operator|(CrtpExprBase<device, Rhs_Derived, void, PTuple> &&exp) {
+    return CrtpExprBase<device, Rhs_Derived, decltype(*this), PTuple>(
+        *this, exp.getArgs())
+  }
+};
 
-  template <typename Rhs_Derived>
-  auto operator|(CrtpExprBase<device, Rhs_Derived> &&exp) {}
+template <Device device, typename Derived, typename PreExpr,
+          typename ParamTuple>
+class CrtpExprBase {
+private:
+  ParamTuple _args;
+  PreExpr _pre;
+
+public:
+  CrtpExprBase() : _args(std::tuple<>()) {}
+
+  CrtpExprBase(PreExpr &&pre_expr, ParamTuple &&params)
+      : _pre(std::forward<PreExpr>(pre_expr)), _args(params) {}
+
+  Derived *cast_to() { return static_cast<Derived *>(this); }
+
+  template <typename... Args> auto eval(Args &&...args) {
+    return cast_to()->run(std::forward<Args>(args)...);
+  }
+
+  auto getArgs() { return _args; }
+
+  template <typename... Args> auto operator()(Args &&...args) {
+    auto new_args = std::tuple_cat(
+        _args, std::make_tuple(Meta::ref_helper(std::forward<Args>(args))...));
+    using p_type = decltype(new_args);
+    if constexpr (std::tuple_size_v<p_type> <
+                  Meta::traits<decltype(&Derived::eval)>::param_size)
+      return CrtpExprBase<device, Derived, PreExpr, p_type>(_pre, new_args);
+    else
+      return Meta::tuple_apply(&Derived::eval, cast_to(), new_args);
+  }
+
+  template <typename Rhs_Derived, typename PTuple>
+  auto operator|(CrtpExprBase<device, Rhs_Derived, void, PTuple> &&exp) {
+    return CrtpExprBase<device, Rhs_Derived, decltype(*this), PTuple>(
+        *this, exp.getArgs())
+  }
 };
 
 template <Device device, typename Alloctor, typename EleDataType>
