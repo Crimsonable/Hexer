c08facf277160c42882f3f437ffd1c9525e165de
Crimsonable <broccoliology@163.com>
2022-12-22 17:14:24 +0800

    laplacian smoothing

--- core/deformation.h
+++ core/deformation.h
@@ -3,8 +3,41 @@
 #include "mesh.h"
 
 namespace Hexer {
-    class LaplacianSmoother:public CrtpExprBase<Device::CPU,LaplacianSmoother>{
-        public:
-        static void eval(const OpenVolumeMesh::VertexHandle& vertex,PolyhedralMesh* mesh);
-    };
-}
+class UniformWeighter {
+public:
+  template <typename M, typename V, typename E, typename P>
+  void operator()(std::vector<std::pair<uint, double>> &weights, uint vid,
+                  const cinolib::AbstractMesh<M, V, E, P> &mesh) {
+    for (uint vvid : mesh.adj_v2v(vid))
+      weights.push_back({vvid, 1.0});
+  }
+};
+
+class LaplacianMatrix : public CrtpExprBase<Device::CPU, LaplacianMatrix> {
+public:
+  template <typename Weighter, typename M, typename V, typename E, typename P>
+  static auto eval(Weighter &&weighter,
+                   const cinolib::AbstractMesh<M, V, E, P> &mesh) {
+    auto Laplacian =
+        Eigen::SparseMatrix<double>(mesh.num_verts(), mesh.num_verts());
+    for (uint vid = 0; vid < mesh.num_verts(); ++vid) {
+      std::vector<std::pair<uint, double>> weights;
+      weighter(weights, vid, mesh);
+      double sum = 0.0;
+      for (auto &&w : weights) {
+        sum += w.second;
+        Laplacian.insert(vid, w.first) = -w.second;
+      }
+      Laplacian.insert(vid, vid) = sum;
+    }
+    Laplacian.makeCompressed();
+    return Laplacian;
+  }
+};
+
+class LaplacianSmoother:public CrtpExprBase<Device::CPU,LaplacianSmoother>{
+    public:
+    template<typename M,typename V,typename E,typename P>
+    static auto eval()
+};
+} // namespace Hexer
--- core/mesh.h
+++ core/mesh.h
@@ -10,42 +10,4 @@
 
 namespace Hexer {
 
-template <typename M, typename V, typename E, typename P>
-double uniform_weighter(std::vector<std::pair<uint, double>> &weights, uint vid,
-                        const cinolib::AbstractMesh<M, V, E, P> &mesh) {
-  for (uint vvid : mesh.adj_v2v(vid))
-    weights.push_back({vvid, 1.0});
-}
-
-class dummyclass : public CrtpExprBase<Device::CPU, dummyclass> {
-public:
-  template <typename T> static auto eval(T a) { return 1; }
-};
-
-template <typename Weighter, typename M = cinolib::Mesh_std_attributes,
-          typename V = cinolib::Vert_std_attributes,
-          typename E = cinolib::Edge_std_attributes,
-          typename P = cinolib::Polygon_std_attributes>
-class LaplacianMatrix
-    : public CrtpExprBase<Device::CPU, LaplacianMatrix<Weighter, M, V, E, P>> {
-public:
-  static auto eval(Weighter &&weighter,
-                   const cinolib::AbstractMesh<M, V, E, P> &mesh) {
-    auto Laplacian =
-        Eigen::SparseMatrix<double>(mesh.num_verts(), mesh.num_verts());
-    for (uint vid = 0; vid < mesh.num_verts(); ++vid) {
-      std::vector<std::pair<uint, double>> weights;
-      weighter(weights, vid, mesh);
-      double sum = 0.0;
-      for (auto &&w : weights) {
-        sum += w.second;
-        Laplacian.insert(vid, w.first) = -w.second;
-      }
-      Laplacian.insert(vid, vid) = sum;
-    }
-    Laplacian.makeCompressed();
-    return Laplacian;
-  }
-};
-
 } // namespace Hexer
